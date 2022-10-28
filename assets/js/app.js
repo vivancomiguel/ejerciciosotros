3a0482b0bd7c3c98f4c9a7c4032aa7ed27f536b0
vivancomiguel <113225991+vivancomiguel@users.noreply.github.com>
2022-10-28 13:23:23 -0500

    fix: Arreglo del contenido de los archivos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,39 @@
+/*
+  Cambios
+  Rectifique la forma de tomar el name por #name
+  Renombre las constantes de elementos del DOM
+  Volvi la funcion asincrona
+  Cree una nueva constante que toma el objeto JSON del fetch respuesta
+  Cambie las comillas simples por comillas hacia atras ` en la asignacion del contenido de los elementos DOM
+  Asigne correctamente el elemento DOM del cual sale el error
+ */
+
+// Enlace del API
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// Elementos del DOM
+const $name = document.querySelector('#name');
+const $blog = document.querySelector('#blog');
+const $location = document.querySelector('#location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+// Fetch de la pagina (llamada a una API)
+async function displayUser(username) {
+  $name.textContent = 'cargando...'; // Texto de espera en lo que hace la solicitud fetch
+  const response = await fetch(`${usersEndpoint}/${username}`); // Solicitud fetch
+  const data = await response.json(); // Conversion de la solicitud a un objeto JSON
+  // console.log(data); // Imprime objeto de la respuesta
+  /* Se asigna la informacion de la respuesta a los elementos del DOM */
+  $name.textContent = `${data.name}`;
+  $blog.textContent = `${data.blog}`;
+  $location.textContent = `${data.location}`;
 }
 
+// Respuesta en caso de que falle la solicitud fetch
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $name.textContent = `Algo salió mal: ${err}`
 }
 
+// Funcion que se ejecuta al ser cargado este archivo
 displayUser('stolinski').catch(handleError);
